fd69f032aeb68137ed1afdd7e705231904104336
ahdaqeelkhan <144414970+ahdaqeelkhan@users.noreply.github.com>
2023-09-11 09:21:39 +0530

    Add files via upload

---
+++ card.js
@@ -0,0 +1,15 @@
+//this us the file used to animate event-2.html cards
+
+imgCard=document.querySelectorAll(".img-card");
+
+imgCard.forEach(element => {
+    element.addEventListener("click", function (e) {
+
+        //remove active class
+        imgCard.forEach(el => {
+            el.classList.remove("active")
+        });
+
+        element.classList.add("active");
+    })
+});
